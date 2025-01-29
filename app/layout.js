60a83657c10d993cabba0de6e63e463bcbb25c86
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-01-29 19:05:09 +0530

    user auth using clerk

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import {  Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -18,12 +13,27 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider 
+    appearance={{
+      baseTheme: dark,
+    }}
+    
+    >
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className}`}
       >
+        <Header />
+        <main className="min-h-screen">
         {children}
+        </main>
+        <footer className="bg-blue-50 py-12">
+          <div className="container mx-auto text-center text-gray-600">
+            All rights reserved@2025
+          </div>
+        </footer>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
