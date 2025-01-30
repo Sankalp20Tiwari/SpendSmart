20d280b64d78c6e177e67c8ac084791e2adf8e32
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-01-31 01:07:32 +0530

    transaction table in id page

--- actions/accounts.js
+++ actions/accounts.js
@@ -13,8 +13,19 @@ const serializeTransaction = (obj) =>{
     if(obj.amount){
         serialized.balance = obj.amount.toNumber()
     }
-    return serialized
+    return serialized;
 }
+
+const serializeDecimal = (obj) => {
+    const serialized = { ...obj };
+    if (obj.balance) {
+      serialized.balance = obj.balance.toNumber();
+    }
+    if (obj.amount) {
+      serialized.amount = obj.amount.toNumber();
+    }
+    return serialized;
+  };
 
 export async function updateDefaultAccount(accountId) {
     try {
@@ -55,3 +66,77 @@ export async function updateDefaultAccount(accountId) {
         return {success:false, error:error.message}
     }
 }
+
+
+// export async function getAccountWithTransactions(accountId) {
+    
+//         const {userId} = await auth()
+//         if(!userId) throw new Error("Unauthorized")
+        
+//         const user = await db.user.findUnique({
+//             where:{
+//                     clerkUserId: userId
+//                 }
+//         })
+        
+//         if(!user) throw new Error("User not found")
+
+//         const account = await db.account.findUnique({
+//             where: {
+//                 id: accountId,
+//                 userId: user.id
+//             },
+//             include: {
+//                 transactions:{
+//                     orderBy:{date:"desc"},
+//                 },
+//                 _count:{
+//                     select:{
+//                         transactions:true
+//                     },
+//                 },
+//             },
+//         })
+
+//         if(!account) return null
+//         // console.log(account) 
+        
+//         return {
+//             ...serializeTransaction(account),
+//             transactions: account.transactions.map(serializeTransaction),
+//         }
+// }
+
+export async function getAccountWithTransactions(accountId) {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+  
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+  
+    if (!user) throw new Error("User not found");
+  
+    const account = await db.account.findUnique({
+      where: {
+        id: accountId,
+        userId: user.id,
+      },
+      include: {
+        transactions: {
+          orderBy: { date: "desc" },
+        },
+        _count: {
+          select: { transactions: true },
+        },
+      },
+    });
+  
+    if (!account) return null;
+  
+    return {
+      ...serializeDecimal(account),
+      transactions: account.transactions.map(serializeDecimal),
+    };
+  }
+
