01d490214eaf75cbbca91781a62de7c49d01c45d
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-02-04 20:22:51 +0530

    recurring rtransaction cron job

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,5 @@
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlert } from "@/lib/inngest/functions";
+import { checkBudgetAlert, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
 
@@ -8,6 +8,8 @@ export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     /* your functions will be passed here later! */
-    checkBudgetAlert
+    checkBudgetAlert,
+    triggerRecurringTransactions,
+    processRecurringTransaction
   ],
 });
