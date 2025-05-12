8104f9a0f8638d62b7ede54e9db7b44de0df5b40
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-05-13 01:44:27 +0530

    image config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,18 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname:"randomuser.me"
+            },
+            {
+                protocol: 'https',
+                hostname:"images.unsplash.com"
+            },
+            {
+                protocol: 'https',
+                hostname:"images.pexels.com"
+            },
+            {
+                protocol:'https',
+                hostname:"static.vecteezy.com"
             }
         ]
     },
